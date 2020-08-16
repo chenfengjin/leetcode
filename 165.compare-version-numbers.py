151d31deb53dee7f3c388d4d449e2d3c85d53de0
chenfengjin <chenfengjin@zhihu.com>
2020-08-16 21:41:00 +0800

    RFC: Add 48.rotate-image

--- 165.compare-version-numbers.py
+++ 165.compare-version-numbers.py
@@ -7,6 +7,17 @@
 # @lc code=start
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        
+        version1 = [int(i) for i in version1.split(".")]
+        version2 = [int(i) for i in version2.split(".")]
+        if len(version1) > len(version2):
+            version2.extend([0]*(len(version1) - len(version2)))
+        if len(version1) < len(version2):
+            version1.extend([0]*(len(version2)-len(version1)))    
+        for v1,v2 in zip(version1,version2):
+            if v1 > v2:
+                return 1
+            if v1 < v2:
+                return -1
+        return 0    
 # @lc code=end
 
