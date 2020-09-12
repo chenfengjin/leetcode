e06332c3ead2adec0ceb9993ad280353db700a86
chenfengjin <chenfengjin@zhihu.com>
2020-09-12 18:36:16 +0800

    RFC: 219.contains-duplicate-ii.py

--- 219.contains-duplicate-ii.py
+++ 219.contains-duplicate-ii.py
@@ -5,8 +5,26 @@
 #
 
 # @lc code=start
+from typing import * 
+# 用哈希表，而不是集合
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        nums_table = set()
+        for num in nums[0:k]:
+            if num in nums_table:
+                return True
+            nums_table.add(num)
+
+        for index in range(k,len(nums)):
+            if nums[index]  in nums_table:
+                return True
+            nums_table.add(nums[index])
+            if nums[index-k] in nums_table:
+                nums_table.remove(nums[index-k])
+        return False
         
 # @lc code=end
 
+
+if __name__ == "__main__":
+    print(Solution().containsNearbyDuplicate([1,2,3,1],3))
