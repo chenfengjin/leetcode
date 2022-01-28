3c0507509f993d043291de7ee5770f87380ed7f7
chenfengjin <1871653365@qq.com>
2022-01-28 13:27:51 +0800

    add 102.binary-tree-level-order-traversal.go

---
+++ 102.binary-tree-level-order-traversal.go
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=102 lang=golang
+ *
+ * [102] Binary Tree Level Order Traversal
+ */
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// @lc code=start
+/**
+* Definition for a binary tree node.
+* type TreeNode struct {
+*     Val int
+*     Left *TreeNode
+*     Right *TreeNode
+* }
+ */
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+	curLayerNode := []*TreeNode{root}
+	result := [][]int{}
+	for len(curLayerNode) != 0 {
+		curLayerValue := []int{}
+		nextLayerNode := []*TreeNode{}
+		for _, node := range curLayerNode {
+			curLayerValue = append(curLayerValue, node.Val)
+			if node.Left != nil {
+				nextLayerNode = append(nextLayerNode, node.Left)
+			}
+			if node.Right != nil {
+				nextLayerNode = append(nextLayerNode, node.Right)
+			}
+		}
+		result = append(result, curLayerValue)
+		curLayerNode = nextLayerNode
+	}
+	return result
+}
+
+// @lc code=end
