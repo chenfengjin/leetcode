35ac00dfbaafda41d3a7a69bb6722823bfab675a
chenfengjin <1871653365@qq.com>
2022-01-30 18:48:37 +0800

    add 231.power-of-two.go

--- 231.power-of-two.go
+++ 231.power-of-two.go
@@ -8,16 +8,17 @@ package main
 
 // @lc code=start
 func isPowerOfTwo(n int) bool {
-	if n == 0 {
-		return false
-	}
-	if n < 0 {
-		return false
-	}
-	for n&1 == 0 {
-		n >>= 1
-	}
-	return n == 1 || n == -1
+	return n != 0 && n&(n-1) == 0
+	// if n == 0 {
+	// 	return false
+	// }
+	// if n < 0 {
+	// 	return false
+	// }
+	// for n&1 == 0 {
+	// 	n >>= 1
+	// }
+	// return n == 1 || n == -1
 }
 
 // @lc code=end
