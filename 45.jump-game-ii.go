b0f888a2a8d56693ae65084ac660b29dfcaeb9f1
chenfengjin <1871653365@qq.com>
2022-02-13 17:38:53 +0800

    update 45.jump-game-ii.go

--- 45.jump-game-ii.go
+++ 45.jump-game-ii.go
@@ -4,33 +4,44 @@
  * [45] Jump Game II
  */
 package main
-import "fmt"
-// pay attention to conner case 
-// stop of j should be nums[i] rather than nums[i]-1
-// using negative to take advantage of initializatin of slice 
+
+import (
+	"fmt"
+	"math"
+)
+
+// pay attention to conner case
 // @lc code=start
 func jump(nums []int) int {
 	// 2,3,1,1,4
 	// 0 1 1 2 2
 	steps := make([]int, len(nums))
-	// fmt.Println(len(steps))
-	steps[0] = 1
+	for i := 0; i < len(steps); i++ {
+		steps[i] = math.MaxInt32
+	}
+	if len(nums) == 1 {
+		return 0
+	}
+	steps[0] = 0
+
 	for i := 0; i < len(nums); i++ {
 		if nums[i] == 0 {
 			continue
 		}
-		for j := 1; j <=nums[i]; j++ {
-			// small trick to use initialization of slice
-			if i+j< len(steps) && -1*steps[i+j] >  -1*(nums[i]+1){
-				steps[i+j] = -1* (nums[i]+1)
+		for j := 1; j <= nums[i]; j++ {
+			if j+i >= len(nums) {
+				break
+			}
+			if steps[i+j] > steps[i]+1 {
+				steps[i+j] = steps[i] + 1
 			}
 		}
 	}
-	return steps[len(steps)-1]*-1
+	return steps[len(steps)-1]
 }
 
 // @lc code=end
 
-func main(){
-	fmt.Println(jump([]int{2,3,1,1,4}))
+func main() {
+	fmt.Println(jump([]int{2, 3, 1, 1, 4}))
 }
