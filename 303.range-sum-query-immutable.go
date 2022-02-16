3b73ebaa99abafe68a15bfe9eb35b7e6413011d2
chenfengjin <1871653365@qq.com>
2022-02-17 00:24:49 +0800

    add 303.range-sum-query-immutable.go

---
+++ 303.range-sum-query-immutable.go
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=303 lang=golang
+ *
+ * [303] Range Sum Query - Immutable
+ */
+package main
+
+// @lc code=start
+type NumArray struct {
+	preSum []int
+}
+
+func Constructor(nums []int) NumArray {
+	if len(nums) == 0 {
+		return NumArray{}
+	}
+	preSum := []int{nums[0]}
+	for i := 1; i < len(nums); i++ {
+		preSum = append(preSum, preSum[i-1]+nums[i])
+	}
+	return NumArray{
+		preSum: preSum,
+	}
+
+}
+
+func (this *NumArray) SumRange(left int, right int) int {
+	if left == 0 {
+		return this.preSum[right]
+	}
+	return this.preSum[right] - this.preSum[left-1]
+
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.SumRange(left,right);
+ */
+// @lc code=end
