2a19aa192a90dc57121c51894f8f82f8c290da47
chenfengjin <1871653365@qq.com>
2022-02-27 18:46:24 +0800

    add 951.flip-equivalent-binary-trees.go

---
+++ 951.flip-equivalent-binary-trees.go
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=951 lang=golang
+ *
+ * [951] Flip Equivalent Binary Trees
+ */
+package main
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func flipEquiv(root1 *TreeNode, root2 *TreeNode) bool {
+	if root1 == nil && root2 == nil {
+		return true
+	}
+	if root1 == nil || root2 == nil {
+		return false
+	}
+	if root1.Val != root2.Val {
+		return false
+	}
+	return (flipEquiv(root1.Left, root2.Left) && flipEquiv(root1.Right, root2.Right)) ||
+		(flipEquiv(root1.Left, root2.Right) && flipEquiv(root1.Right, root2.Left))
+}
+
+// @lc code=end
